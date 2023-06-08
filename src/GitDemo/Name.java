b75b483118698e5eb952927d16ab26123ff23423
21lij <21lij@Lijo_John>
2023-06-08 12:22:27 +0400

    first commit.

---
+++ src/GitDemo/Name.java
@@ -0,0 +1,14 @@
+package GitDemo;
+
+public class Name {
+	public static void printName(String name){
+		System.out.println("name "+name);
+		}
+
+
+		public static void main(String args[]){
+
+			Name.printName("simi");
+		}
+
+}
